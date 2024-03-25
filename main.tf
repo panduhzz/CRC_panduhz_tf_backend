56d8d0c0faffd59ea21eff5ab276134e85c131a0
panduhzz <christopherchannn@gmail.com>
2024-03-25 16:48:32 -0700

    added resource group, cosmosdb account, table, app service plan. Still need to work on the function and connecting them together

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+  }
+}
+resource "azurerm_resource_group" "backend-rg" {
+  name     = "panduhz_backend_rg"
+  location = "westus2"
+}
+resource "azurerm_cosmosdb_account" "panduhz-db" {
+  name                = "panduhz-counter-coosmosdb"
+  location            = azurerm_resource_group.backend-rg.location
+  resource_group_name = azurerm_resource_group.backend-rg.name
+  offer_type          = "Standard"
+  geo_location {
+    location          = "westus2"
+    failover_priority = 0
+    zone_redundant    = false
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 86400
+    max_staleness_prefix    = 1000000
+  }
+}
+resource "azurerm_cosmosdb_table" "panduhz-tbl" {
+  name                = "panduhz-table"
+  resource_group_name = azurerm_cosmosdb_account.panduhz-db.resource_group_name
+  account_name        = azurerm_cosmosdb_account.panduhz-db.name
+  throughput          = 400
+}
+#need app service plan for linux function app
+resource "azurerm_app_service_plan" "panduhzsrvc" {
+  name                = "api-appserviceplan-pro"
+  location            = azurerm_resource_group.backend-rg.location
+  resource_group_name = azurerm_resource_group.backend-rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Y1"
+    size = "Y1"
+  }
+}
