e4d84fe04021a914a592ec34bba322947ba67bee
panduhzz <christopherchannn@gmail.com>
2024-04-08 10:17:44 -0700

    using serverless for CosmosDB Account

--- main.tf
+++ main.tf
@@ -26,7 +26,8 @@ resource "azurerm_cosmosdb_account" "panduhz-db" {
   name                = "panduhz-counter-cosmosdb"
   location            = azurerm_resource_group.backend-rg.location
   resource_group_name = azurerm_resource_group.backend-rg.name
-  offer_type          = "Standard"
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
   geo_location {
     location          = "westus2"
     failover_priority = 0
@@ -48,7 +49,6 @@ resource "azurerm_cosmosdb_table" "panduhz-tbl" {
   name                = "azurerm"
   resource_group_name = azurerm_cosmosdb_account.panduhz-db.resource_group_name
   account_name        = azurerm_cosmosdb_account.panduhz-db.name
-  throughput          = 400
 }
 
 resource "azurerm_log_analytics_workspace" "workspace" {
