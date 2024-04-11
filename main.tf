8afac9d56de0452cdd2de8a97209f11e574b6d1f
Christopher Chan <christopherchannn@gmail.com>
2024-04-11 12:36:50 -0700

    configuring logic app to send slack messages as action group

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "azurerm_logic_app_trigger_http_request" "slack_trigger" {
 }
 SCHEMA
 }
-variable "TF_VAR_SLACK_BOT_TOKEN" {
+variable "SLACK_BOT_TOKEN" {
   type        = string
   description = "Token for authenticating requests to Slack"
   sensitive   = true
@@ -109,7 +109,7 @@ resource "azurerm_logic_app_action_http" "post_to_slack" {
   uri          = "https://slack.com/api/chat.postMessage"
 
   headers = {
-    "Authorization" = "Bearer ${var.TF_VAR_SLACK_BOT_TOKEN}"
+    "Authorization" = "Bearer ${var.SLACK_BOT_TOKEN}"
     "Content-Type"  = "application/json"
   }
 
